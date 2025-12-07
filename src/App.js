c0214b5eb021f8bec1b411aecba53c9b7cecd606
humamT <humam.01@hotmail.com>
2025-12-08 00:45:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useEffect, useState } from "react";
+import "./App.css"; // we’ll add CSS here
+
+function Designs() {
+  const [designs, setDesigns] = useState([]);
+
+  useEffect(() => {
+    fetch("http://ned-toukhi.local/wp-json/wp/v2/posts")
+      .then((res) => res.json())
+      .then((data) => setDesigns(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="designs-container">
+      {designs.map((design) => (
+        <div className="design-card" key={design.id}>
+          <h2>{design.title.rendered}</h2>
+          {design.featured_media_url && (
+            <img src={design.featured_media_url} alt={design.title.rendered} />
+          )}
+          <div
+            className="design-content"
+            dangerouslySetInnerHTML={{ __html: design.content.rendered }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default Designs;
+
